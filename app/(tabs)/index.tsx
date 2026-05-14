209f8027d4c70c964375143c20bfb6b66f92458c
Dennis Demir <dd222gc@student.lnu.se>
2026-05-14 21:54:06 +0200

    feat: Project setup, nativewind, routing, tav navigation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,14 @@
-import { View, Text } from "react-native";
+import { Text } from "react-native";
 import "../../global.css";
 import { Link } from "expo-router";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+import {styled} from "nativewind";
+
+const SafeAreaView = styled(RNSafeAreaView);
 
 export default function App() {
     return (
-        <View className="flex-1 items-center justify-center bg-background">
+        <SafeAreaView className="flex-1 bg-background p-5">
             <Text>Index</Text>
             <Link href="/onboarding">Go to onboarding</Link>
             <Link href="/subscriptions/spotify">Go to spotify subscription</Link>
@@ -19,6 +23,6 @@ export default function App() {
             >
                 Claude Max Subscriptiosn
             </Link>
-        </View>
+        </SafeAreaView>
     )
 }
